4cba4300a2606a03ccd2951337dc824436016127
nikolis <nikolis@pop-os.localdomain>
2022-03-22 22:18:45 +0200

    comment cache static manifest

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :mehungry_web, MehungryWeb.Endpoint,
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  #cache_static_manifest: "priv/static/cache_manifest.json"
 
 
 IO.inspect("Config -----------------------------------------------------------------------------------------------------------")
