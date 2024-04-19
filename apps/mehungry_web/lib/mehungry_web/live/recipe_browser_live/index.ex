f735fbc4c0c6a2eb3a5b8e52fa4c7eacc50b2ea5
nikolis <>
2024-04-19 18:20:55 +0300

    intermidiate

--- apps/mehungry_web/lib/mehungry_web/live/recipe_browser_live/index.ex
+++ apps/mehungry_web/lib/mehungry_web/live/recipe_browser_live/index.ex
@@ -101,6 +101,7 @@ defmodule MehungryWeb.RecipeBrowseLive.Index do
       |> Search.change_recipe_search_item(recipe_search_item_params)
       |> Map.put(:action, :validate)
 
+    IO.inspect(changeset, label: "asdfacasdadfsadfsadfsadfsadfsasdfas")
     {:noreply,
      socket
      |> assign(:changeset, changeset)}
@@ -113,7 +114,22 @@ defmodule MehungryWeb.RecipeBrowseLive.Index do
   end
 
   def handle_event("search", %{"recipe_search_item" => %{"query_string" => query_string}}, socket) do
-    {:noreply, Phoenix.LiveView.push_navigate(socket, to: "/browse")}
+    {result, cursor_after} = Food.search_recipe(query_string)
+    
+    result =
+      Enum.map(result, fn recipe ->
+        return = ImageProcessing.resize(recipe.image_url, 100, 100)
+        %Recipe{recipe | recipe_image_remote: return}
+      end)
+
+    {result, cursor_after}
+
+     {:noreply, socket
+     |> assign(:cursor_after, cursor_after)
+     |> assign(:page, 1)
+     |> assign(:recipes, result)}
+
+
   end
 
   def handle_event(
@@ -135,7 +151,7 @@ defmodule MehungryWeb.RecipeBrowseLive.Index do
 
       {:ok, %RecipeSearchItem{} = recipe_search_item} ->
         recipes = Search.search_recipe(recipe_search_item.query_string)
-
+        IO.inspect(recipes, label: "=================================================================================================================")
         {:noreply,
          socket
          |> assign(recipes: recipes)}
@@ -226,10 +242,8 @@ defmodule MehungryWeb.RecipeBrowseLive.Index do
           nil
       end
 
-    # IO.inspect(mufa_all)
     nuts_pre = [mufa_all, pufa_all, sfa_all, tfa_all, vitamins_all]
     nuts_pre = Enum.filter(nuts_pre, fn x -> !is_nil(x) end)
-    # IO.inspect(nuts_pre, label: "nuts tree")
 
     nuts_pre =
       Enum.map(nuts_pre, fn x ->
@@ -246,9 +260,6 @@ defmodule MehungryWeb.RecipeBrowseLive.Index do
 
     nutrients = nuts_pre ++ rest
     nutrients = Enum.filter(nutrients, fn x -> !is_nil(x) end)
-    # IO.inspect(length(rest), label: "Total")
-    # IO.inspect(rest, label: "rEST")
-    # IO.inspect(nutrients, label: "nutrients")
 
     socket
     |> assign(:page_title, "Recipe Details")
@@ -278,12 +289,6 @@ defmodule MehungryWeb.RecipeBrowseLive.Index do
               Map.put(user_content, to_string(@user_id), new_list)
           end
       end
-
-    # case Cachex.put(:users, "data", new_content) do
-    # {:ok, true} ->
-
-    # {errorm, reason} ->
-    # end
 
     {:noreply, assign(socket, :recipes, list_recipes())}
   end
