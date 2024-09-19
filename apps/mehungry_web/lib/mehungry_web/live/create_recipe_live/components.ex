a1a10de5419240a31d8b0802849f26e40d2fccf7
nikolis <>
2024-09-19 17:26:46 +0300

    fixing update recipe view in create_recipe_live

--- apps/mehungry_web/lib/mehungry_web/live/create_recipe_live/components.ex
+++ apps/mehungry_web/lib/mehungry_web/live/create_recipe_live/components.ex
@@ -16,7 +16,11 @@ defmodule MehungryWeb.CreateRecipeLive.Components do
       case is_nil(Phoenix.HTML.Form.input_value(assigns.g, :ingredient_id)) do
         true ->
           if(length(assigns.f.data.recipe_ingredients) > 0) do
-            Enum.at(assigns.f.data.recipe_ingredients, assigns.g.index).id
+            if is_nil(Enum.at(assigns.f.data.recipe_ingredients, assigns.g.index)) do 
+              nil 
+            else
+              Enum.at(assigns.f.data.recipe_ingredients, assigns.g.index).id
+            end
           else
             nil
           end
