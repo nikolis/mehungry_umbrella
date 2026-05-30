79fde88388884194d06317b47ad01b28eeaf8aeb
nikolis <>
2026-05-30 17:59:45 +0300

    config chagnes

--- config/runtime.exs
+++ config/runtime.exs
@@ -27,3 +27,20 @@ if config_env() == :prod do
 
   config :swoosh, :api_client, Swoosh.ApiClient.Hackney
 end
+
+if config_env() == :prod do
+  database_url =
+    System.get_env("DATABASE_URL") ||
+      raise """
+        environment variable DATABASE_URL is missing.
+      """
+
+  maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
+  query_args = ["SET pg_trgm.similarity_threshold = 0.3", []]
+
+  config :mehungry, Mehungry.Repo,
+    url: database_url,
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+    socket_options: maybe_ipv6,
+    after_connect: {Postgrex, :query!, query_args}
+end
